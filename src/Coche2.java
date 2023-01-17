056a025d43f2021da61c06be686a7a5c08ea2446
Adrian <avideirarial@danielcastelao.org>
2023-01-17 19:38:43 +0100

    nueva clase

---
+++ src/Coche2.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Coche2 {
+
+        private int ruedas;
+        private int largo;
+        private int ancho;
+        private int motor;
+        private int peso_plataforma;
+        private String color;
+        int peso_total;
+        boolean asientos_cuero, climatizador;
+
+}
