cac6bbae7cf9e48c6b4b98a870820be770392b63
Adrian <avideirarial@danielcastelao.org>
2023-01-17 19:28:03 +0100

    se añadio edad

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("Me llamo Adrian Videira Rial");
+        System.out.println("Tengo 33 años");
     }
 }
